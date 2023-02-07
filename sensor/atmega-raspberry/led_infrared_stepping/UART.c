67241b050b839c043ed1a53a2ce3b8a75bcd1c93
hh <ndg5778@gmail.com>
2023-02-07 12:16:21 +0900

    uart 스테핌모터 + 적외선센서

---
+++ sensor/atmega-raspberry/led_infrared_stepping/UART.c
@@ -0,0 +1,25 @@
+﻿#include "UART.h"
+
+void UART_INIT(void) {
+	UCSR0A |= _BV(U2X0);
+
+	UBRR0H = 0x00;
+	UBRR0L = 207;
+
+	UCSR0C |= 0x06;
+	
+	UCSR0B |= _BV(RXEN0);
+	UCSR0B |= _BV(TXEN0);
+}
+
+unsigned char UART_receive(void)
+{
+	while( !(UCSR0A & (1<<RXC0)) );
+	return UDR0;
+}
+
+void UART_transmit(unsigned char data)
+{
+	while( !(UCSR0A & (1<<UDRE0)) );
+	UDR0 = data;
+}
---
+++ sensor/atmega-raspberry/led_infrared_stepping/UART.h
@@ -0,0 +1,10 @@
+﻿#ifndef _UART_H_
+#define _UART_H_
+
+#include <avr/io.h>
+
+void UART_INIT(void);
+unsigned char UART_receive(void);
+void UART_transmit(unsigned char data);
+
+#endif
