cfca56b323940628c98db8b5c284e521e5d3b19f
hh <ndg5778@gmail.com>
2023-02-24 09:19:51 +0900

    230224

--- robot_arm_v1/robot_arm_v1/test.c
+++ robot_arm_v1/robot_arm_v1/test.c
@@ -11,45 +11,24 @@
 //#define LED1 PB0
 //#define LED2 PB1
 //
-//void INIT_SERVO(){
-	//
-	//pca9685_init(0x00, 51); // start PCA9685 device 0x00 at 50 Hz output
-	//
-	//_delay_ms(5);
-	//
-	//int i;
-	//
-	//for (i = 0; i < 5; i++){
-		//pca9685_pwm(i, 1300);
-	//}
-//}
 //
 //int main(void) {
-	//LED_DDR |= (1 << LED1) | (1 << LED2);
-	//DDRB &= ~(1 << switch1);
+	////LED_DDR |= (1 << LED1) | (1 << LED2);
+	////DDRB &= ~(1 << switch1);
+	//
+	//DDRB = 0xFF;
+	//DDRC = 0xFF;
+	//DDRD = 0xFF;
 	//
 	//PORTB |= (1 << LED1);
-	//INIT_SERVO();
 	//
 	//_delay_ms(1000);
 	//
+	//
 	//while (1) {
-		////INIT_SERVO();
-		//int angle;
-		//for (angle = 1800; angle <= 600; angle -= 20) {
-			//PORTB |= 0x02;
-			//pca9685_pwm(3, angle);
-			//_delay_ms(20);
-		//}
-		//for (angle = 600; angle <= 1800; angle += 20) {
-			//PORTB |= 0x02;
-			////pca9685_pwm(0, angle);
-			////pca9685_pwm(1, angle);
-			////pca9685_pwm(2, angle);
-			////pca9685_pwm(3, angle);
-			//pca9685_pwm(4, angle);
-			//_delay_ms(100);
-		//}
-		//_delay_ms(50);
+		//PORTB = 0xFF;
+		//PORTC = 0xFF;
+		//PORTD = 0xFF;
+		//
 	//}
 //}
