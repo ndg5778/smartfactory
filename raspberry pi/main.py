75c02e425f94fe09f404b79e23078fdcae8df5e1
hh <ndg5778@gmail.com>
2023-03-06 22:17:13 +0900

    230306

--- raspberry pi/main.py
+++ raspberry pi/main.py
@@ -23,94 +23,95 @@ def uart_canmove():
     stop_receive = 1
     
 
-
 while True:
-    data = ser.readline().decode().strip()
-    if (data == "====First Robot Arm===="):
-        print(f"1. Receivede data: {data}")
-        
-        stop_receive = 1
-        
-        while (stop_receive != 0):
+    try:
+        data = ser.readline().decode().strip()
+        if (data == "====First Robot Arm===="):
+            print(f"1. Receivede data: {data}")
             
-            #### database 연결
-            #print("num : ", end="")
-            #print(ser.write(f'{str(num)}$'.encode('utf-8')))
-            ## print(f'num : {num}')
+            stop_receive = 1
             
-            num = int(input("몇 번째 어쩌구 : "))
+            while (stop_receive != 0):
+                
+                #### database 연결
+                #print("num : ", end="")
+                #print(ser.write(f'{str(num)}$'.encode('utf-8')))
+                ## print(f'num : {num}')
+                
+                num = int(input("몇 번째 어쩌구 : "))
 
-            ser.write(f'{num}$'.encode())
-            print(f'{num}$'.encode())
+                ser.write(f'{num}$'.encode())
+                print(f'{num}$'.encode())
 
-            time.sleep(1)
-            
-            data = ser.readline().decode().strip()
-            
-            if ("pass" in data):
-                print(f">pass// num : {num}, data : {data}")
-                # print(f"num : {num}")
-                # print("move!!")
-                # num += 1
-                stop_receive = 0
-            elif ("end" in data):
-                print(f">end// num : {num}, data : {data}")
-                # print(f"num : {num}")
-                #print("stop!!")
-                stop_receive = 0
-            elif (data == "===First Robot Arm==="):
-                pass
-            else:
-                print(f"what is Received data?: {data}")
-
-            print(f"{data}")
-            # time.sleep(1)
-
-    elif (data == "====First Infrared===="):
-        print(f"Received data: {data}")
-        pass_infrared = 1
-        count = 0
-
-        while(pass_infrared != 0):
-            state = GPIO.input(infrared_A)
-            if (state == 0):
-                print("box is on the convayor")
-                ser.write(b'go$')
-                pass_infrared = 0
-
-            elif (state == 1):
-                count += 1
-                print(count)
                 time.sleep(1)
                 
-            if (count >= 10):
-                ser.write(b'stp$')
-                print("stop conveyor")
-                pass_infrared = 0
+                data = ser.readline().decode().strip()
+                
+                if ("pass" in data):
+                    print(f">pass// num : {num}, data : {data}")
+                    # print(f"num : {num}")
+                    # print("move!!")
+                    # num += 1
+                    stop_receive = 0
+                elif ("end" in data):
+                    print(f">end// num : {num}, data : {data}")
+                    # print(f"num : {num}")
+                    #print("stop!!")
+                    stop_receive = 0
+                elif (data == "===First Robot Arm==="):
+                    pass
+                else:
+                    print(f"what is Received data?: {data}")
 
-    elif (data == "====stepping motor===="):
-        print(f"Received data: {data}")
-        stop_receive = 1
-        while (stop_receive != 0):
-            t = time.localtime()
-            
-            state_B = GPIO.input(infrared_B)
-            if (state_B == 0):
-                ser.write(b'mov$')
-                print(f"{t.tm_hour}:{t.tm_min}:{t.tm_sec} - send data : {state_B}")
-                # print(f"send data A : {state_B}")
+                print(f"{data}")
                 # time.sleep(1)
-                stop_receive = 2
-            elif (stop_receive == 2 and state_B == 1):
-                ser.write(b'mov$')
-            elif (state_B == 0):
-                ser.write(b'stp$')
-                for i in range(10):
-                    print(f"send data B : {state_B}")
-                stop_receive = 0
-    elif(data == ""):
-        pass
-    else:
-        print(f"Received other data: {data}")
 
-    
+        elif (data == "====First Infrared===="):
+            print(f"Received data: {data}")
+            pass_infrared = 1
+            count = 0
+
+            while(pass_infrared != 0):
+                state = GPIO.input(infrared_A)
+                if (state == 0):
+                    print("box is on the convayor")
+                    ser.write(b'go$')
+                    pass_infrared = 0
+
+                elif (state == 1):
+                    count += 1
+                    print(count)
+                    time.sleep(1)
+                    
+                if (count >= 10):
+                    ser.write(b'stp$')
+                    print("stop conveyor")
+                    pass_infrared = 0
+
+        elif (data == "====stepping motor===="):
+            print(f"Received data: {data}")
+            stop_receive = 1
+            while (stop_receive != 0):
+                t = time.localtime()
+                
+                state_B = GPIO.input(infrared_B)
+                if (state_B == 0):
+                    ser.write(b'stp$')
+                    print(f"{t.tm_hour}:{t.tm_min}:{t.tm_sec} - send data : {state_B}")
+                    # print(f"send data A : {state_B}")
+                    time.sleep(1)
+                    stop_receive = 0
+                # elif (stop_receive == 2 and state_B == 1):
+                #     ser.write(b'mov$')
+                elif (state_B == 1):
+                    ser.write(b'go$')
+                    print(f"{t.tm_hour}:{t.tm_min}:{t.tm_sec} - send data : {state_B}")
+                    # print(f"send data B : {state_B}")
+                    # time.sleep(0.05)
+        elif(data == ""):
+            pass
+        else:
+            print(f"Received other data: {data}")
+
+    except UnicodeDecodeError:
+        pass
