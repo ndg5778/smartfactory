eb684adea3a4a0af9df42dc81123c594ebb0201d
hh <ndg5778@gmail.com>
2023-03-06 10:06:44 +0900

    230306

--- robot_arm_v1/robot_arm_v1/atmega_led_test.c
+++ robot_arm_v1/robot_arm_v1/atmega_led_test.c
@@ -1,16 +1,16 @@
-﻿#define F_CPU 16000000L
-#include <avr/io.h> // include the AVR IO library
-
-
-int main(void){
-	DDRB = 0xFF;
-	DDRC = 0xFF;
-	DDRD = 0xFF;
-	
-	while (1) {
-		PORTB = 0xFF;
-		PORTC = 0xFF;
-		PORTD = 0xFF;
-	}
-	
-}
+﻿//#define F_CPU 16000000L
+//#include <avr/io.h> // include the AVR IO library
+//
+//
+//int main(void){
+	//DDRB = 0xFF;
+	//DDRC = 0xFF;
+	//DDRD = 0xFF;
+	//
+	//while (1) {
+		//PORTB = 0xFF;
+		//PORTC = 0xFF;
+		//PORTD = 0xFF;
+	//}
+	//
+//}
