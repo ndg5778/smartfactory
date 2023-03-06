17c35ea716c525068699209d14f4805bcb2a77d8
hh <ndg5778@gmail.com>
2023-03-06 18:05:55 +0900

    230306

--- raspberry pi/main.py
+++ raspberry pi/main.py
@@ -7,8 +7,8 @@ ser = serial.Serial(port = '/dev/ttyAMA0',
                     timeout = 1)
 
 # 적외선센서 GPIO pin
-infrared_A = 18
-infrared_B = 20
+infrared_A = 23
+infrared_B = 24
 
 # 변수 선언
 num = 0
@@ -38,6 +38,8 @@ while True:
             #print(ser.write(f'{str(num)}$'.encode('utf-8')))
             ## print(f'num : {num}')
             
+            num = int(input("몇 번째 어쩌구 : "))
+
             ser.write(f'{num}$'.encode())
             print(f'{num}$'.encode())
 
@@ -48,8 +50,8 @@ while True:
             if ("pass" in data):
                 print(f">pass// num : {num}, data : {data}")
                 # print(f"num : {num}")
-                #print("move!!")
-                num += 1
+                # print("move!!")
+                # num += 1
                 stop_receive = 0
             elif ("end" in data):
                 print(f">end// num : {num}, data : {data}")
@@ -64,15 +66,39 @@ while True:
             print(f"{data}")
             # time.sleep(1)
 
+    elif (data == "====First Infrared===="):
+        print(f"Received data: {data}")
+        pass_infrared = 1
+        count = 0
+
+        while(pass_infrared != 0):
+            state = GPIO.input(infrared_A)
+            if (state == 0):
+                print("box is on the convayor")
+                ser.write(b'go$')
+                pass_infrared = 0
+
+            elif (state == 1):
+                count += 1
+                print(count)
+                time.sleep(1)
+                
+            if (count >= 10):
+                ser.write(b'stp$')
+                print("stop conveyor")
+                pass_infrared = 0
+
     elif (data == "====stepping motor===="):
         print(f"Received data: {data}")
         stop_receive = 1
         while (stop_receive != 0):
-            state_A = GPIO.input(infrared_A)
+            t = time.localtime()
+            
             state_B = GPIO.input(infrared_B)
-            if (state_A == 0):
+            if (state_B == 0):
                 ser.write(b'mov$')
-                print(f"send data A : {state_A}")
+                print(f"{t.tm_hour}:{t.tm_min}:{t.tm_sec} - send data : {state_B}")
+                # print(f"send data A : {state_B}")
                 # time.sleep(1)
                 stop_receive = 2
             elif (stop_receive == 2 and state_B == 1):
