40d44bac08d297f18177678131630fcdd1e4919e
hh <ndg5778@gmail.com>
2023-03-23 17:52:34 +0900

    230323

--- raspberry pi/uart_led_test.py
+++ raspberry pi/uart_led_test.py
@@ -1,7 +1,9 @@
 import serial
+import time
 
 # Set up the serial port
 ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
+time.sleep(1)
 
 num = -1
 
@@ -10,12 +12,15 @@ while (num != 0):
     num = int(input("1:ON 2:OFF 0:EXIT : "))
 
     if (num == 1):
+        ser.flush()
         ser.write(b'h')
         print("h")
     elif (num == 2):
+        ser.flush()
         ser.write(b'l')
         print("l")
     elif (num == 3):
+        ser.flush()
         ser.write(b'f')
         print("f")
     elif (num == 0):
